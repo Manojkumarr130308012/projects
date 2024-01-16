b552a20a164287484a8e6c6c0d5dfc5026f128e4
manojthoffen <manojkumar.t@hoffensoft.com>
2024-01-16 13:32:44 +0530

    merchant link updated

--- router/merchant.js
+++ router/merchant.js
@@ -1,24 +1,24 @@
 const router = require('express').Router();
-const businessController = require('../controller/business');
+const merchantController = require('../controller/merchant');
 
 
 router.post('/add', async (req, res) => {
-    res.send(await businessController.add(req.body));
+    res.send(await merchantController.add(req.body));
 });
 router.get('/', async (req, res) => {
-    res.send(await businessController.fetch());
+    res.send(await merchantController.fetch());
 });
 router.get('/fetchlorders', async (req, res) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
-	const response = await businessController.fetchdata(req.query.id);
+	const response = await merchantController.fetchdata(req.query.id);
 	res.send(response);
 })
 router.delete('/delete', async (req, res) => {
-	const response = await businessController.delete(req.query.id);
+	const response = await merchantController.delete(req.query.id);
 	res.send(response);
 })
 router.put('/update', async (req, res) => {
-	const response = await businessController.update(req.query.id, req.body);
+	const response = await merchantController.update(req.query.id, req.body);
 	res.send(response);
 })
 
