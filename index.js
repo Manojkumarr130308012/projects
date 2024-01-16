d3ad669295f62c93867d572cefc6cb332b10d312
manojthoffen <manojkumar.t@hoffensoft.com>
2024-01-16 19:32:12 +0530

    add to firebase

--- index.js
+++ index.js
@@ -8,6 +8,20 @@ const paths = require("./config");
 const fs = require('fs');
 const axios = require('axios');
 const { config } = require("dotenv");
+const admin = require('firebase-admin');
+var serviceAccount = require("./admin.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://letschat-f9f77.firebaseio.com",
+});
+
+var storage = admin.storage();
+
+const bucket = storage.bucket('gs://letschat-f9f77.appspot.com');
+
+
+
 
 let jsonData;
 app.use(
@@ -24,7 +38,8 @@ paths.forEach((element) => {
 
 
 
-const middleware=require('./middleware/middleware')
+const middleware=require('./middleware/middleware');
+const { Console } = require("console");
 app.use(middleware);
 
 
@@ -77,6 +92,74 @@ app.post("/producth", async (req, res) => {
   });
 
 });
+
+
+
+app.post("/producth/write1", async (req, res) => {
+  const file = bucket.file('app_settings.json');
+
+  file.download()
+    .then((data) => {
+      let combinedJson;
+      if(data.toString()){
+        const jsonContent = JSON.parse(data[0].toString());
+        if(!jsonContent){
+          console.log("null",jsonContent);
+          combinedJson = req.body;
+        }else{
+           combinedJson = {...jsonContent,...req.body};
+        }
+        console.log('JSON Content:', combinedJson);
+  
+        const jsonString = JSON.stringify(combinedJson, null, 2); // The '2' argument adds indentation for better readability
+  
+        file.save(jsonString)
+        .then(() => {
+           res.send("JSON file saved to Firebase Storage.")
+        })
+        .catch((error) => {
+          res.send(error)
+        });
+      }else{
+        combinedJson = req.body;
+
+        console.log("combinedJson",combinedJson)
+        file.save(JSON.stringify(combinedJson))
+        .then(() => {
+           res.send("JSON file saved to Firebase Storage.")
+        })
+        .catch((error) => {
+          console.log("error",error)
+
+          res.send(error)
+        });
+      }
+
+    })
+    .catch((error) => {
+      res.send(error)
+    });
+
+});
+
+
+app.post("/producth/read1", async (req, res) => {
+  const file = bucket.file('app_settings.json');
+
+  file.download()
+    .then((data) => {
+      console.log(data)
+
+      const jsonContent = JSON.parse(data[0].toString());
+      console.log(jsonContent)
+      res.send( jsonContent)
+    })
+    .catch((error) => {
+      res.send(error);
+        });
+
+});
+
 
 
 app.post("/producth/write", async (req, res) => {
