b1b9248be21864f497cf2561a721320a3c35e488
manojthoffen <manojkumar.t@hoffensoft.com>
2024-01-25 12:44:39 +0530

    testing

--- config.js
+++ config.js
@@ -1,27 +1,27 @@
 let paths = [
   {
     service: "login",
-    host: "http://localhost:3001/user/login",
+    host: "https://producth-client.web.app/user/login",
     method:"POST"
   },
   {
     service: "65a64145d9f7c5001dc569b0",
-    host: "http://localhost:3001/user/register",
+    host: "https://producth-client.web.app/user/register",
     method:"POST"
   },
   {
     service: "65a64145d9f7c5001dc569b0",
-    host: "http://localhost:3001/user",
+    host: "https://producth-client.web.app/user",
     method:"GET"
   },
   {
     service: "65a64145d9f7c5001dc569b0",
-    host: "http://localhost:3001/user/update",
+    host: "https://producth-client.web.app/user/update",
     method:"PUT"
   },
   {
     service: "65a64145d9f7c5001dc569b0",
-    host: "http://localhost:3001/user/delete",
+    host: "https://producth-client.web.app/user/delete",
     method:"DELETE"
   }
 ];
